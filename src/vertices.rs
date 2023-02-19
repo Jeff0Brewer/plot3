26b58a62b3eea263bfe5ab84fece097447945e86
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-02-19 16:38:21 -0700

    add texture vertex with pos and tcoord

--- src/vertices.rs
+++ src/vertices.rs
@@ -1,13 +1,24 @@
-// sized position vertex with C repr for gl buffering
-type Pos = [f32; 3];
-#[repr(C, packed)]
-pub struct PosVert(pub Pos);
+type Position = [f32; 3];
+type TexCoord = [f32; 2];
 
-// macro to remove init boilerplate
+#[repr(C)]
+pub struct PosVert(pub Position);
+// convert Nx3 array into PosVert vec
 macro_rules! pos_vert {
     ($($pos:expr),*) => {
         vec![$(PosVert($pos),)*]
     }
 }
-
 pub(crate) use pos_vert;
+
+#[repr(C, packed)]
+pub struct TexVert(pub Position, pub TexCoord);
+// convert Nx5 array into TexVert arr
+macro_rules! tex_vert {
+    ($([$a:expr, $b:expr, $c:expr, $d:expr, $e:expr]),*) => {
+        [$(
+            TexVert([$a, $b, $c], [$d, $e]),
+        )*]
+    }
+}
+pub(crate) use tex_vert;
