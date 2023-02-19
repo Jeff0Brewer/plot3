ea7b7bdceff0d9a9b0a8aeda900e830c5d411397
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-02-19 14:02:09 -0700

    remove plot object, create window for text display testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+#![allow(dead_code)] // for text module testing
+
 mod vertices;
 mod plot;
 mod axis_vert;
 mod axis;
 mod scene;
 mod gl_wrap;
-use plot::Plot;
+use gl_wrap::{Window};
+use scene::{Scene};
+extern crate gl;
 
 fn main() {
-    let plot = Plot::new("test", 800.0, 800.0).unwrap();
-    plot.display().unwrap();
+    let test_window = Window::new("text testing", 800.0, 800.0).unwrap();
+    let scene = Scene::new_empty();
+    unsafe { gl::ClearColor(0.0, 0.0, 0.1, 1.0); }
+
+    test_window.run(vec![scene]);
 }
+
+//use plot::Plot;
+//let plot = Plot::new("test", 800.0, 800.0).unwrap();
+//plot.display().unwrap();
