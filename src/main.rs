34478b4f1e46302c729e4f07095120c5fe309c3b
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-02-19 15:38:45 -0700

    add fontdue dep and font file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)] // for text module testing
 
+extern crate gl;
+extern crate fontdue;
 mod vertices;
 mod plot;
 mod axis_vert;
@@ -8,14 +10,22 @@ mod scene;
 mod gl_wrap;
 use gl_wrap::{Window};
 use scene::{Scene};
-extern crate gl;
+use fontdue::{Font, FontSettings};
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
 
 fn main() {
-    let test_window = Window::new("text testing", 800.0, 800.0).unwrap();
-    let scene = Scene::new_empty();
-    unsafe { gl::ClearColor(0.0, 0.0, 0.1, 1.0); }
-
-    test_window.run(vec![scene]);
+    //let test_window = Window::new("text testing", 800.0, 800.0).unwrap();
+    //let scene = Scene::new_empty();
+    //unsafe { gl::ClearColor(0.0, 0.0, 0.1, 1.0); }
+    //test_window.run(vec![scene]);
+    let font_bytes = include_bytes!("../resources/Roboto-Regular.ttf") as &[u8];
+    let font = Font::from_bytes(font_bytes, FontSettings::default()).unwrap();
+    let (metrics, bitmap) = font.rasterize('O', 15.0);
+    print_type_of(&bitmap);
+    println!("{}", bitmap.len());
 }
 
 //use plot::Plot;
