f72a664c132f431f0e33580e0603a4dd36b73cf8
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-02-20 13:37:18 -0700

    init font map generation

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -1,17 +1,61 @@
+extern crate fontdue;
 use crate::vertices::{BitmapVert, bmp_vert};
+use crate::gl_wrap::{Program};
+use fontdue::{Font, FontSettings};
+use std::fs;
 
-struct Bitmap {
-    vertices: [BitmapVert; 4]
+pub struct Bitmap {
+    program: Program,
+    vertices: [BitmapVert; 4],
+    chars: Vec<char>
 }
 
 impl Bitmap {
-    pub fn new() -> Self {
+    pub fn new() -> Result<Self, BitmapError> {
+        let program = Program::new_from_files(
+            "./shaders/bitmap_vert.glsl",
+            "./shaders/bitmap_frag.glsl"
+        )?;
         let vertices = bmp_vert![
-            [1.0, 1.0, 1.0, 1.0],
-            [1.0, -1.0, 1.0, 0.0],
-            [-1.0, -1.0, 0.0, 0.0],
-            [-1.0, 1.0, 0.0, 1.0]
+            [0.5, 0.5, 1.0, 1.0],
+            [0.5, -0.5, 1.0, 0.0],
+            [-0.5, -0.5, 0.0, 0.0],
+            [-0.5, 0.5, 0.0, 1.0]
         ];
-        Self { vertices }
+        let chars: Vec<char> = CHAR_SET.chars().collect();
+        Ok(
+            Self { program, vertices, chars }
+        )
     }
+
+    pub fn gen_font_map(&self, font_file: &str) -> Result<(), BitmapError> {
+        let font_bytes = &fs::read(font_file)? as &[u8];
+        let font_result = Font::from_bytes(font_bytes, FontSettings::default());
+        // verbose error check since FontResult returns &str for error type
+        if let Err(err) = font_result {
+            return Err(BitmapError::FontError(err.to_string()));
+        }
+        let font = font_result.unwrap();
+        for &c in &self.chars {
+            let (metrics, bitmap) = font.rasterize(c, FONT_SIZE);
+        }
+        Ok(())
+    }
+}
+
+static CHAR_SET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+static FONT_SIZE: f32 = 20.0;
+
+extern crate thiserror;
+use thiserror::Error;
+use crate::gl_wrap::{ProgramError};
+
+#[derive(Error, Debug)]
+pub enum BitmapError {
+    #[error("{0}")]
+    ProgramError(#[from] ProgramError),
+    #[error("{0}")]
+    IoError(#[from] std::io::Error),
+    #[error("{0}")]
+    FontError(String)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ mod scene;
 mod gl_wrap;
 mod bitmap;
 use plot::Plot;
+use bitmap::Bitmap;
 
 fn main() {
     let plot = Plot::new("test", 800.0, 800.0).unwrap();
+    let bmp = Bitmap::new().unwrap();
+    bmp.gen_font_map("./resources/Roboto-Regular.ttf").unwrap();
     plot.display().unwrap();
 }
