9510c06bc06d11d28946026519f0ac79f021779b
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-02-27 12:42:36 -0700

    add field names to vertex types

--- src/vertices.rs
+++ src/vertices.rs
@@ -1,27 +1,44 @@
 #[repr(C)]
-pub struct PosVert(pub [f32; 3]);
-// convert Nx3 array into PosVert vec
-macro_rules! pos_vert {
-    ($($pos:expr),*) => {
-        vec![$(PosVert($pos),)*]
-    }
+pub struct PosVert {
+    pub position: [f32; 3]
 }
-pub(crate) use pos_vert;
 
 #[derive(Copy, Clone)]
 #[repr(C, packed)]
-pub struct BitmapVert(pub [f32; 2], pub [f32; 2]);
+pub struct BitmapVert {
+    pub position: [f32; 2],
+    pub texcoord: [f32; 2]
+}
+
+// convert Nx3 array into PosVert vec
+macro_rules! pos_vert {
+    ($($pos:expr),*) => {
+        vec![$(PosVert{
+            position: $pos
+        },)*]
+    }
+}
+
 // convert Nx4 array into BitmapVert arr
 macro_rules! bmp_arr {
     ($([$a:expr, $b:expr, $c:expr, $d:expr]),*) => {
-        [$(BitmapVert([$a, $b], [$c, $d]),)*]
+        [$(BitmapVert{
+            position: [$a, $b],
+            texcoord: [$c, $d]
+        },)*]
     }
 }
-pub(crate) use bmp_arr;
 
+// convert Nx4 array into BitmapVert vec
 macro_rules! bmp_vert {
     ($([$a:expr, $b:expr, $c:expr, $d:expr]),*) => {
-        vec![$(BitmapVert([$a, $b], [$c, $d]),)*]
+        vec![$(BitmapVert{
+            position: [$a, $b],
+            texcoord: [$c, $d]
+        },)*]
     }
 }
+
+pub(crate) use pos_vert;
+pub(crate) use bmp_arr;
 pub(crate) use bmp_vert;
