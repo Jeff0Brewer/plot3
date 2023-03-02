65b9a75ea1206dabd1cce80eff00b3a585b05105
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-01 17:35:29 -0700

    remove arrow axis style, adjust tick color based on style

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ mod gl_wrap;
 mod font_map;
 mod label_draw;
 use plot::Plot;
-use axis::{BorderStyle, TickStyle};
+use axis::TickStyle;
 
 fn main() {
     let mut plot = Plot::new("test", 800.0, 800.0).unwrap();
     plot.set_background_color([0.05, 0.05, 0.05]);
-    plot.axis.set_border_style(BorderStyle::Box);
-    plot.axis.set_tick_style(TickStyle::Grid);
+    plot.axis.set_tick_style(TickStyle::Tick);
+    plot.axis.set_border_color([1.0, 1.0, 1.0]);
+    plot.axis.set_tick_color([0.5, 0.5, 0.5]);
     plot.set_bounds(1.0, 0.5, 0.7);
     plot.labels.set_font_face("./resources/Ubuntu-Regular.ttf").unwrap();
     plot.labels.set_labels("X axis", "Y axis", "Z axis");
